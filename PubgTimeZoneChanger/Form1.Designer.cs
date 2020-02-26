d17f7fd84b460c8c278ab89cb57d3d75cc6c1093
hiroki19990625 <hiroki19990625@gmail.com>
2020-02-27 01:35:09 +0900

    完成

--- PubgTimeZoneChanger/Form1.Designer.cs
+++ PubgTimeZoneChanger/Form1.Designer.cs
@@ -29,12 +29,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 11F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(246, 165);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 80, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(293, 26);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.button1.Location = new System.Drawing.Point(246, 322);
+            this.button1.Margin = new System.Windows.Forms.Padding(240, 2, 240, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "変更";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label1.Location = new System.Drawing.Point(10, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(606, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NowServerText";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.label2.Location = new System.Drawing.Point(87, 166);
+            this.label2.Margin = new System.Windows.Forms.Padding(80, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "接続するサーバー";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(626, 377);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "PubgTimeZoneChanger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- PubgTimeZoneChanger/Form1.cs
+++ PubgTimeZoneChanger/Form1.cs
@@ -2,19 +2,98 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PubgTimeZoneChanger.Properties;
 
 namespace PubgTimeZoneChanger
 {
     public partial class Form1 : Form
     {
+        private const string BackupFile = "time_zone_bk.txt";
+
         public Form1()
         {
             InitializeComponent();
+
+            label1.Text = string.Format(Resources.form_now_server, ConvertServerName(TimeZoneInfo.Local));
+
+            comboBox1.Items.Add("日本サーバー");
+            comboBox1.Items.Add("アジアサーバー");
+            comboBox1.Items.Add("元に戻す");
+
+            comboBox1.SelectedIndex = 0;
+
+            if (!File.Exists(BackupFile))
+            {
+                File.WriteAllText(BackupFile, TimeZoneInfo.Local.Id);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("コンピュータの時間が変更されてしまいますが、変更しますか？", "注意", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                string locName = ConvertFromServerName(comboBox1.Items[comboBox1.SelectedIndex].ToString());
+                ProcessStartInfo startInfo =
+                    new ProcessStartInfo("tzutil.exe",
+                        $"/s \"{locName}\"");
+                startInfo.RedirectStandardOutput = true;
+                startInfo.UseShellExecute = false;
+                Process process = Process.Start(startInfo);
+
+                process?.WaitForExit();
+
+                if (process?.ExitCode == 0)
+                {
+                    MessageBox.Show("成功しました!", "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    label1.Text = string.Format(Resources.form_now_server,
+                        ConvertServerName(TimeZoneInfo.FindSystemTimeZoneById(locName)));
+                }
+                else
+                {
+                    MessageBox.Show(process?.StandardOutput.ReadToEnd(), "エラー", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private string ConvertServerName(TimeZoneInfo timeZoneInfo)
+        {
+            string stdName = timeZoneInfo.Id.Replace("_dstoff", "");
+            switch (stdName)
+            {
+                case "Tokyo Standard Time":
+                    return "日本サーバー";
+                case "China Standard Time":
+                    return "アジアサーバー";
+                default:
+                    return "その他";
+            }
+        }
+
+        private string ConvertFromServerName(string serverName)
+        {
+            switch (serverName)
+            {
+                case "日本サーバー":
+                    return "Tokyo Standard Time";
+                case "アジアサーバー":
+                    return "China Standard Time";
+                case "元に戻す":
+                    return File.ReadAllText(BackupFile);
+                default:
+                    throw new Exception("変換に失敗");
+            }
         }
     }
 }
